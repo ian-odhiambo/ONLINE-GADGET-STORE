39a29af24196d244d40a4047f0afc50ce28e1dea
ian <odhiamboian096@gmail.com>
2026-04-15 22:28:33 +0300

    commit

--- frontend/src/COMPONENTS/Navbar.jsx
+++ frontend/src/COMPONENTS/Navbar.jsx
@@ -1,8 +1,13 @@
-import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+
 import { AiOutlinePlusSquare } from "react-icons/ai";
+import { IoMoon } from "react-icons/io5";
+import {LuSun} from "react-icons/lu";
 
 const Navbar = () => {
+  const{ colorMode,toggleColorMode } = useColorMode();  
+
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
@@ -21,7 +26,7 @@ const Navbar = () => {
           textAlign={"center"}
           bgGradient={"linear(to-r, cyan.400, purple.500)"}
           bgClip={"text"}
-          color={"transparent"}
+         
         >
           <Link to={"/"}>Product Store 🛒</Link>
         </Text>
@@ -31,6 +36,9 @@ const Navbar = () => {
               <AiOutlinePlusSquare fontSize={20} />
             </Button>
           </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon/> : <LuSun/>}
+          </Button>
         </HStack>
       </Flex>
     </Container>
@@ -38,3 +46,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+//<IoMoon/> : <LuSun/>
