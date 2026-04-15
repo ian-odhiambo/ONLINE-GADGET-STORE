4cc8eed8c8295a596138511cd4124c238e5f254d
ian <odhiamboian096@gmail.com>
2026-04-15 18:01:17 +0300

    commit

--- frontend/src/COMPONENTS/Navbar.jsx
+++ frontend/src/COMPONENTS/Navbar.jsx
@@ -2,7 +2,23 @@ import { Container } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <Container maxW={"1140px"}>Navbar</Container>
+    <Container maxW={"1140px"} px={4}>        
+        <Flex
+            h={16}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            flexDirection={{
+                base: "column",
+                sm: "row"
+            }}
+        >
+            <Text
+            
+            >
+
+            </Text>
+        </Flex>
+    </Container>
   )
 }
 
