e7044287760a3d27e0cc5c4ccf1b3bf524d97b06
ian <odhiamboian096@gmail.com>
2026-04-15 21:50:18 +0300

    commit

--- frontend/src/COMPONENTS/Navbar.jsx
+++ frontend/src/COMPONENTS/Navbar.jsx
@@ -1,39 +1,40 @@
 import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
-import {Link} from "react-router-dom";
-import { PlusSquareIcon } from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
+import { AiOutlinePlusSquare } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <Container maxW={"1140px"} px={4}>        
-        <Flex
-            h={16}
-            alignItems={"center"}
-            justifyContent={"space-between"}
-            flexDirection={{
-                base: "column",
-                sm: "row"
-            }}
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDirection={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, purple.500)"}
+          bgClip={"text"}
+          color={"transparent"}
         >
-            <Text
-                fontSize={{base: "22", sm: "28"}}
-                fontWeight={"bold"}
-                textTransform={"uppercase"}
-                textAlign={"center"}
-                bgGradient={"linear(to-r, cyan.400, purple.500)"}
-                bgClip={"text"}
-            >
-              <Link to={"/"}>Product Store  🛒</Link>               
-            </Text>
-            <HStack spacing={2} alignItems={"center"}>
-                <Link to={"/create"}>
-                    <Button>
-                        <PlusSquareIcon/>
-                    </Button>
-                </Link>
-            </HStack>    
-        </Flex>
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <AiOutlinePlusSquare fontSize={20} />
+            </Button>
+          </Link>
+        </HStack>
+      </Flex>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import App from './App.jsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider,  } from '@chakra-ui/react';
 import {BrowserRouter} from "react-router-dom";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
