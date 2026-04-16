0eb8a214a81348599f848a0031c2a715e22cfdb1
ian <odhiamboian096@gmail.com>
2026-04-16 17:44:07 +0300

    another comit

---
+++ frontend/src/store/product.js
@@ -0,0 +1,8 @@
+import {create} from 'zustand';
+
+export const useProductStore = create((set) =>({
+    products: [],
+    setProducts:(products) => set({ products })
+}))
+
+const[state, setState] = useState
