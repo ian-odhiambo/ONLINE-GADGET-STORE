9b8002edd9da59ed7edc51addc0698eba5cac078
ian <odhiamboian096@gmail.com>
2026-04-16 23:18:42 +0300

    first commit

--- frontend/src/PAGES/HomePage.jsx
+++ frontend/src/PAGES/HomePage.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Container, VStack , Text} from '@chakra-ui/react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+        fontSize={"30"}
+        fontWeight={"bold"}
+        bgGradient={"linear(to-r, cyan.400, blue.500)"}
+        textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
+
+        <Text 
+        fontSize="xl" 
+        textAlign={"center"} 
+        fontWeight={"bold"} 
+        color="gray.500"
+        >
+          No Products Found 😢{""}
+          <Link to="/create">
+            <Text as="span" color="blue.500" _hover= {{textDecoration: "underline"}}>
+              Create A Product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
   )
 }
 
