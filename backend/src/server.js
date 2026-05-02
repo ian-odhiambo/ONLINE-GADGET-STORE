966b3ddf397c78466573940a670ce97b815417a0
ian <odhiamboian096@gmail.com>
2026-05-02 23:31:41 +0300

    another commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,31 @@
-import express from "express";
-import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import path from "path";
+// import express from "express";
+// import dotenv from "dotenv";
+// import { connectDB } from "./config/db.js";
+// import path from "path";
 
-import productRoutes from "./routes/v1/productsRoute.js";
+// import productRoutes from "./routes/v1/productsRoute.js";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+// const app = express();
+// const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-app.use(express.json()); // allows us to accept JSON data in the body
+// const __dirname = path.resolve();
+// app.use(express.json()); // allows us to accept JSON data in the body
 
-app.use("/api/v1/products", productRoutes);
+// app.use("/api/v1/products", productRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../../frontend", "dist", "index.html"),
-    );
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "../../frontend", "dist", "index.html"),
+//     );
+//   });
+// }
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`server started http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   connectDB();
+//   console.log(`server started http://localhost:${PORT}`);
+// });
