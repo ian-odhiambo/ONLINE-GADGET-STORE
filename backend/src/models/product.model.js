4558cbe3b55887244ef8aa1c9012b72db4edf523
ian <odhiamboian096@gmail.com>
2026-05-18 20:48:18 +0300

    second commit of the day

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required:true
-    },
-    price:{
-        type: Number,
-        required:true
-    },
-    image:{
-        type: String,
-        required:true
-    },
-},{
-    timestamps: true // Ensures it has the created at amd the updated at fields
-});
+// const productSchema = new mongoose.Schema({
+//     name:{
+//         type: String,
+//         required:true
+//     },
+//     price:{
+//         type: Number,
+//         required:true
+//     },
+//     image:{
+//         type: String,
+//         required:true
+//     },
+// },{
+//     timestamps: true // Ensures it has the created at amd the updated at fields
+// });
 
-const Product = mongoose.model('Product', productSchema);
+// const Product = mongoose.model('Product', productSchema);
 
-export default Product;
+// export default Product;
