2f4144a7ba80ca98fdd1db2557bcb441426a6d4a
ian <odhiamboian096@gmail.com>
2026-05-18 20:46:11 +0300

    another commit

--- backend/src/routes/v1/productsRoute.js
+++ backend/src/routes/v1/productsRoute.js
@@ -1,17 +1,17 @@
-// import express from "express";
-// import {
-//    getProducts,
-//    createProduct,
-//    updateProduct,
-//    deleteProduct
-// } from "../../controllers/productsController.js"
+import express from "express";
+import {
+   getProducts,
+   createProduct,
+   updateProduct,
+   deleteProduct
+} from "../../controllers/productsController.js"
 
 
-// const router = express.Router()
+const router = express.Router()
 
-// router.get("/", getProducts);
-// router.post("/", createProduct);
-// router.put("/:id", updateProduct);
-// router.delete("/:id", deleteProduct);
+router.get("/", getProducts);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
-// export default router;
+export default router;
